991da16e897727a97d52fddfd7f920b2954d4b08
Luis Eduardo M. <luiseduardomc019@gmail.com>
2024-05-07 16:03:12 -0300

    SaveAndQuit, Cut, Save

--- src/Editor.java
+++ src/Editor.java
@@ -7,11 +7,13 @@ public class Editor {
   private Scanner scanner;
   private String cmd;
   private String[] selectedLines;
+  private boolean isModified;
 
   Editor(String filePath) {
     file = new FileHandler(filePath);
     scanner = new Scanner(System.in);
     cmd = "";
+    isModified = false;
   }
 
   // :e nomeArq.ext
@@ -19,6 +21,12 @@ public class Editor {
     file = new FileHandler(filePath);
     file.ReadFile();
     // file.GetDLL().Print();
+  }
+
+  // :wq
+  void SaveAndExit() {
+    Save();
+    System.exit(0);
   }
 
   // :v LinIni LinFim
@@ -37,13 +45,13 @@ public class Editor {
     while (currentNode != null) {
       if (lineNumber >= start && lineNumber <= end) {
         String line = currentNode.getData();
-        selectedLines[index++] = line; // Salva a linha no array
+        selectedLines[index++] = line; // Linhas selecionadas salvas no array
       }
       currentNode = currentNode.getNext();
       lineNumber++;
     }
 
-    // System.out.println("Linhas selecionadas: " + start + " a " + end);
+    System.out.println("Linhas selecionadas: " + start + " a " + end);
     // for (String line : selectedLines) {
     // System.out.println(line);
     // }
@@ -57,6 +65,49 @@ public class Editor {
     }
 
     // TODO: Copiar as linhas selecionadas
+  }
+
+  // :c
+  void Cut() {
+    if (selectedLines == null || selectedLines.length == 0) {
+      System.out.println("Nenhuma linha selecionada!");
+      return;
+    }
+
+    int start = Integer.MAX_VALUE;
+    int end = Integer.MIN_VALUE;
+
+    Node currentNode = file.GetDLL().GetHead();
+    int lineNumber = 1;
+    while (currentNode != null) {
+      String line = currentNode.getData();
+      for (int i = 0; i < selectedLines.length; i++) {
+        if (line.equals(selectedLines[i])) {
+          start = Math.min(start, lineNumber); // Update start index
+          end = Math.max(end, lineNumber); // Update end index
+          break;
+        }
+      }
+      currentNode = currentNode.getNext();
+      lineNumber++;
+    }
+
+    file.GetDLL().RemoveFrom(start, end);
+    selectedLines = null;
+    isModified = true;
+    System.out.println("Linhas cortadas.");
+
+    // file.GetDLL().Print();
+  }
+
+  // :ZZ
+  void Save() {
+    if (!isModified) {
+      System.out.println("Nenhuma modificacao realizada.");
+      return;
+    }
+
+    file.SaveFile();
   }
 
   // :q!
@@ -97,12 +148,24 @@ public class Editor {
           }
           break;
 
+        case ":wq":
+          SaveAndExit();
+          break;
+
         case ":v":
           if (parts.length >= 3) { // Check second and third arguments for line numbers
             SelectLines(Integer.parseInt(parts[1]), Integer.parseInt(parts[2]));
           } else {
             System.out.println("Usage: :v LinIni LinFim");
           }
+          break;
+
+        case ":c":
+          Cut();
+          break;
+
+        case ":ZZ":
+          Save();
           break;
 
         case ":help":
