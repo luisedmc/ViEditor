c4761d97e12606602612658abc2e7fac49b17982
Luis Eduardo M. <luiseduardomc019@gmail.com>
2024-05-26 16:37:50 -0300

    final

--- src/App.java
+++ src/App.java
@@ -7,3 +7,15 @@ public class App {
         e.InitMenu();
     }
 }
+
+/*
+ References:
+    - https://www.callicoder.com/java-read-file/
+    - https://www.digitalocean.com/community/tutorials/java-read-file-line-by-line
+    - https://www.geeksforgeeks.org/java-program-to-rename-a-file/
+    - https://www.geeksforgeeks.org/data-structures/linked-list/doubly-linked-list/
+    - https://stackoverflow.com/questions/12200514/how-can-i-split-a-string-in-to-multiple-parts
+    - https://www.geeksforgeeks.org/split-string-java-examples/
+    - https://www.geeksforgeeks.org/search-an-element-in-a-linked-list-iterative-and-recursive/
+
+ */
